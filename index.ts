f82014e6fae0d69330b063e39e285bdfd9cc28df
Amin Marashi <me@amin.codes>
2024-07-08 00:00:00 +0000

    chore: convert TF stack to Pulumi

---
+++ index.ts
@@ -0,0 +1,322 @@
+/*
+  Imported from TF state using the following commands
+  $ terraform state pull > terraform.tfstate
+  $ pulumi import --from terraform terraform.tfstate
+*/
+import * as pulumi from "@pulumi/pulumi";
+import * as aws from "@pulumi/aws";
+
+const lambdaRunUser = new aws.iam.User(
+  "lambda_run_user",
+  { name: "lambda-run-user" },
+  {
+    protect: true,
+  }
+);
+const lambdaUserAccessKey = new aws.iam.AccessKey(
+  "lambda_user_access_key",
+  {
+    status: "Active",
+    user: lambdaRunUser.id,
+  },
+  {
+    protect: true,
+  }
+);
+const feedEventsBucket = new aws.s3.BucketV2(
+  "feed_events_bucket",
+  {
+    bucket: "lit-feed-dev-feed-events-bucket",
+    grants: [
+      {
+        id: "af84821e7f22b2a9f90d6ec79dfe537c05e56f02d08875ada641428ededabfc4",
+        permissions: ["FULL_CONTROL"],
+        type: "CanonicalUser",
+        uri: "",
+      },
+    ],
+    lifecycleRules: [
+      {
+        abortIncompleteMultipartUploadDays: 0,
+        enabled: true,
+        expirations: [],
+        id: "manage-storage-lifecycle",
+        noncurrentVersionExpirations: [],
+        noncurrentVersionTransitions: [],
+        prefix: "",
+        tags: {},
+        transitions: [
+          {
+            days: 365,
+            storageClass: "GLACIER",
+          },
+          {
+            days: 30,
+            storageClass: "ONEZONE_IA",
+          },
+        ],
+      },
+    ],
+    requestPayer: "BucketOwner",
+    serverSideEncryptionConfigurations: [
+      {
+        rules: [
+          {
+            applyServerSideEncryptionByDefaults: [
+              {
+                kmsMasterKeyId: "",
+                sseAlgorithm: "AES256",
+              },
+            ],
+            bucketKeyEnabled: false,
+          },
+        ],
+      },
+    ],
+    versionings: [
+      {
+        enabled: false,
+        mfaDelete: false,
+      },
+    ],
+  },
+  {
+    protect: true,
+  }
+);
+const invokeProcessArticlePolicy = new aws.iam.Policy(
+  "invoke_process_article_policy",
+  {
+    description: "Allows calling the process-article Lambda function",
+    name: "invoke-process-article-policy",
+    policy: JSON.stringify({
+      Statement: [
+        {
+          Action: "lambda:InvokeFunction",
+          Effect: "Allow",
+          Resource:
+            "arn:aws:lambda:eu-west-1:058264093352:function:process-article",
+        },
+      ],
+      Version: "2012-10-17",
+    }),
+  },
+  {
+    protect: true,
+  }
+);
+const invokeSyncFeedDatabasePolicy = new aws.iam.Policy(
+  "invoke_sync_feed_database_policy",
+  {
+    description: "Allows calling the sync-feed-database Lambda function",
+    name: "invoke-sync-feed-database-policy",
+    policy: JSON.stringify({
+      Statement: [
+        {
+          Action: "lambda:InvokeFunction",
+          Effect: "Allow",
+          Resource:
+            "arn:aws:lambda:eu-west-1:058264093352:function:sync-feed-database",
+        },
+      ],
+      Version: "2012-10-17",
+    }),
+  },
+  {
+    protect: true,
+  }
+);
+const syncFeedDatabaseLambdaLogGroup = new aws.cloudwatch.LogGroup(
+  "sync_feed_database_lambda_log_group",
+  {
+    logGroupClass: "STANDARD",
+    name: "/aws/lambda/sync-feed-database",
+    retentionInDays: 7,
+  },
+  {
+    protect: true,
+  }
+);
+const syncFeedDatabase = new aws.lambda.Function(
+  "sync_feed_database",
+  {
+    architectures: ["x86_64"],
+    environment: {
+      variables: {
+        MONGO_URL: "/lit-feed/dev/mongo",
+      },
+    },
+    ephemeralStorage: {
+      size: 512,
+    },
+    handler: "index.handler",
+    loggingConfig: {
+      logFormat: "Text",
+      logGroup: syncFeedDatabaseLambdaLogGroup.id,
+    },
+    name: "sync-feed-database",
+    packageType: "Zip",
+    role: "arn:aws:iam::058264093352:role/lambda-execution-role",
+    runtime: aws.lambda.Runtime.NodeJS20dX,
+    tracingConfig: {
+      mode: "PassThrough",
+    },
+  },
+  {
+    protect: true,
+  }
+);
+const articleBucket = new aws.s3.BucketV2(
+  "article_bucket",
+  {
+    bucket: "lit-feed-dev-article-bucket",
+    grants: [
+      {
+        id: "af84821e7f22b2a9f90d6ec79dfe537c05e56f02d08875ada641428ededabfc4",
+        permissions: ["FULL_CONTROL"],
+        type: "CanonicalUser",
+        uri: "",
+      },
+    ],
+    lifecycleRules: [
+      {
+        abortIncompleteMultipartUploadDays: 0,
+        enabled: true,
+        expirations: [],
+        id: "manage-storage-lifecycle",
+        noncurrentVersionExpirations: [],
+        noncurrentVersionTransitions: [],
+        prefix: "",
+        tags: {},
+        transitions: [
+          {
+            days: 365,
+            storageClass: "GLACIER",
+          },
+          {
+            days: 30,
+            storageClass: "ONEZONE_IA",
+          },
+        ],
+      },
+    ],
+    requestPayer: "BucketOwner",
+    serverSideEncryptionConfigurations: [
+      {
+        rules: [
+          {
+            applyServerSideEncryptionByDefaults: [
+              {
+                kmsMasterKeyId: "",
+                sseAlgorithm: "AES256",
+              },
+            ],
+            bucketKeyEnabled: false,
+          },
+        ],
+      },
+    ],
+    versionings: [
+      {
+        enabled: false,
+        mfaDelete: false,
+      },
+    ],
+  },
+  {
+    protect: true,
+  }
+);
+const processArticleLambdaLogGroup = new aws.cloudwatch.LogGroup(
+  "process_article_lambda_log_group",
+  {
+    logGroupClass: "STANDARD",
+    name: "/aws/lambda/process-article",
+    retentionInDays: 7,
+  },
+  {
+    protect: true,
+  }
+);
+const processArticle = new aws.lambda.Function(
+  "process_article",
+  {
+    architectures: ["x86_64"],
+    environment: {
+      variables: {
+        MONGO_URL: "/lit-feed/dev/mongo",
+      },
+    },
+    ephemeralStorage: {
+      size: 512,
+    },
+    handler: "index.handler",
+    loggingConfig: {
+      logFormat: "Text",
+      logGroup: processArticleLambdaLogGroup.id,
+    },
+    memorySize: 512,
+    name: "process-article",
+    packageType: "Zip",
+    role: "arn:aws:iam::058264093352:role/lambda-execution-role",
+    runtime: aws.lambda.Runtime.NodeJS20dX,
+    timeout: 30,
+    tracingConfig: {
+      mode: "PassThrough",
+    },
+  },
+  {
+    protect: true,
+  }
+);
+const lambdaExectutionRole = new aws.iam.Role(
+  "lambda_execution_role",
+  {
+    assumeRolePolicy: JSON.stringify({
+      Statement: [
+        {
+          Action: "sts:AssumeRole",
+          Effect: "Allow",
+          Principal: { Service: "lambda.amazonaws.com" },
+        },
+      ],
+      Version: "2012-10-17",
+    }),
+    inlinePolicies: [
+      {
+        name: "s3-access-policy-article-bucket",
+        policy: JSON.stringify({
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Action: ["s3:GetObject", "s3:PutObject"],
+              Effect: "Allow",
+              Resource: "arn:aws:s3:::lit-feed-dev-article-bucket/*",
+            },
+          ],
+        }),
+      },
+      {
+        name: "s3-access-policy-feed-events-bucket",
+        policy: JSON.stringify({
+          Version: "2012-10-17",
+          Statement: [
+            {
+              Action: ["s3:GetObject", "s3:PutObject"],
+              Effect: "Allow",
+              Resource: "arn:aws:s3:::lit-feed-dev-feed-events-bucket/*",
+            },
+          ],
+        }),
+      },
+    ],
+    managedPolicyArns: [
+      "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole",
+      "arn:aws:iam::aws:policy/AmazonSSMReadOnlyAccess",
+    ],
+    name: "lambda-execution-role",
+  },
+  {
+    protect: true,
+  }
+);
