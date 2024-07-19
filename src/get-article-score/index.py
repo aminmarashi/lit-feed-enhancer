8e4862ca0361634b095cedb2af08587e6e9046d8
Amin Marashi <me@amin.codes>
2024-07-19 00:00:00 +0000

    fix: change the fields names to match user articles

--- src/get-article-score/index.py
+++ src/get-article-score/index.py
@@ -27,8 +27,8 @@ def handler(event, context):
     print('Pipeline loaded from S3')
     try:
       s3.download_file(bucket_name, key, pipeline_full_filename)
-    except:
-      print(f'Could not find a pipeline for user {userId}')
+    except Exception as e:
+      print(f'Could not find a pipeline for user {userId}, error: {e}')
       return {
         'statusCode': 404,
         'body': json.dumps(f'Could not find a pipeline for user {userId}')
@@ -36,7 +36,7 @@ def handler(event, context):
   pipeline = joblib.load(pipeline_full_filename)
   ## Todo handle when there is no pipeline in s3
 
-  article['textcontent'] = article['textcontent'].fillna('').astype(str)
+  article['content'] = article['content'].fillna('').astype(str)
   article['tags'] = article['tags'].fillna('').astype(str)
   article['summary'] = article['summary'].fillna('').astype(str)
   article['title'] = article['title'].fillna('').astype(str)
@@ -54,28 +54,29 @@ def handler(event, context):
 
 if __name__ == '__main__':
   result = handler({
-    'articleid': '65e1909c15e55e8deb1fd47e',
-    'feedurl': 'https://hnrss.org/newest?points=100',
+    'articleId': '65e1909c15e55e8deb1fd47e',
+    'feedUrl': 'https://hnrss.org/newest?points=100',
     'href': 'https://twitter.com/nixcraft/status/1763124892986474689',
-    'textcontent': 'article url: https://twitter.com/nixcraft/status/1763124892986474689\n' +
+    'content': 'article url: https://twitter.com/nixcraft/status/1763124892986474689\n' +
       'comments url: https://news.ycombinator.com/item?id=39558365\n' +
       'points: 162\n' +
       '# comments: 69',
-    'createdat': '2024-03-01t08:23:55.158z',
+    'createdAt': '2024-03-01t08:23:55.158z',
     'date': '2024-03-01t03:53:26.000z',
-    'isread': True,
-    'issaved': False,
+    'isRead': True,
+    'isSaved': False,
     'summary': 'article url: https://twitter.com/nixcraft/status/1763124892986474689\n' +
       'comments url: https://news.ycombinator.com/item?id=39558365\n' +
       'points: 162\n' +
       '# comments: 69',
     'title': 'docusign just admitted that they use customer data to train ai',
-    'updatedat': '2024-05-17t06:02:40.239z',
-    'feedid': '65e03508114dfe73e550d0b3',
-    'feedname': 'hn 100',
-    'synchedat': '2024-03-05t08:53:34.086z',
-    'isliked': None,
+    'updatedAt': '2024-05-17t06:02:40.239z',
+    'feedId': '65e03508114dfe73e550d0b3',
+    'feedName': 'hn 100',
+    'synchedAt': '2024-03-05t08:53:34.086z',
+    'isLiked': None,
     'userId': '65a90719332e28717a201fef',
     'tags': None,
+    'openDuration': 8578928
   }, None)
   print(result)
