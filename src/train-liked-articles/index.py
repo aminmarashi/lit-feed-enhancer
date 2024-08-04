af2dad4c267f693e44f40676b86d35080732810d
Amin Marashi <me@amin.codes>
2024-08-04 00:00:00 +0000

    fix: better weights for dislike, add test mode

--- src/train-liked-articles/index.py
+++ src/train-liked-articles/index.py
@@ -16,11 +16,19 @@ import numpy as np
 from sklearn.utils.class_weight import compute_class_weight
 from imblearn.over_sampling import RandomOverSampler
 
+## TODO: Make these env vars
 bucket_name = 'lit-feed-dev-article-training-data'
 pipeline_filename = 'complete_pipeline.joblib'
+athena_cache_filename = 'athena_cache.csv'
 lambda_tmp_dir = '/tmp'
+athena_cache_full_filename = f'{lambda_tmp_dir}/{athena_cache_filename}'
 pipeline_full_filename = f'{lambda_tmp_dir}/{pipeline_filename}'
-IS_LOCAL = os.environ.get('IS_LOCAL', 'False') == 'True'
+is_test_run = os.environ.get('TEST_RUN', 'False') == 'True'
+testing_sample_fraction = float(os.environ.get('TESTING_SAMPLE_FRACTION', '0.2'))
+like_bias = float(os.environ.get('LIKE_BIAS', '1'))
+dislike_bias = float(os.environ.get('DISLIKE_BIAS', '300'))
+neutral_bias = float(os.environ.get('NEUTRAL_BIAS', '1'))
+print(f"Biased by: like: {like_bias}, dislike: {dislike_bias}, neutral: {neutral_bias}")
 
 def handler(event, context):
   article = event
@@ -46,7 +54,7 @@ def handler(event, context):
   s3 = boto3.client('s3')
   print('Loading pipeline from S3')
   shouldLoadFromScratch = True
-  if not IS_LOCAL:
+  if not is_test_run:
     try:
       if os.path.exists(pipeline_full_filename):
         if os.path.exists(pipeline_full_filename) and (time.time() - os.path.getmtime(pipeline_full_filename)) < 1800:
@@ -76,10 +84,15 @@ def handler(event, context):
     print('Running locally, training will be done from scratch')
   
   if shouldLoadFromScratch:
-    print('Loading training data from Athena')
-    query = f"select distinct b.link, u.title, b.tags, b.textcontent, u.userid, u.issaved, u.isliked, u.isread, b.summary, u.feedurl from default.user_articles u join default.backend_articles b on u.href = b.link where u.userId = '{userId}'"
-    data = wr.athena.read_sql_query(query, database='default')
-    print('loaded data from Athena')
+    if is_test_run and os.path.exists(athena_cache_full_filename):
+      data = pd.read_csv(athena_cache_full_filename)
+      print('loaded data from local athena cache')
+    else:
+      print('Loading training data from Athena')
+      query = f"select distinct b.link, u.title, b.tags, b.textcontent, u.userid, u.issaved, u.isliked, u.isread, b.summary, u.feedurl from default.user_articles u join default.backend_articles b on u.href = b.link where u.userId = '{userId}'"
+      data = wr.athena.read_sql_query(query, database='default')
+      data.to_csv(athena_cache_full_filename, index=False)
+      print('loaded data from Athena')
   else:
     print('Training data loaded from the event')
     data = pd.DataFrame([article])
@@ -87,6 +100,11 @@ def handler(event, context):
   data['textcontent'] = data['textcontent'].fillna('').astype(str)
   data['tags'] = data['tags'].fillna('').astype(str)
   data['summary'] = data['summary'].fillna('').astype(str)
+
+  if is_test_run:
+    print(f"Testing sample fraction: {testing_sample_fraction}")
+    data_for_testing = data.sample(frac=testing_sample_fraction)
+    data = data.drop(data_for_testing.index)
 
   y = data['isliked'].apply(lambda x: 0 if pd.isna(x) or x == None else 1 if x else -1)
   # If issaved is True, set y to 1
@@ -115,6 +133,9 @@ def handler(event, context):
     numpy_classes = np.array(classes)
     class_weights = compute_class_weight(class_weight='balanced', classes=numpy_classes, y=y)
     class_weights_dict = dict(zip(numpy_classes, class_weights))
+    class_weights_dict[-1] *= dislike_bias
+    class_weights_dict[1] *= like_bias
+    class_weights_dict[0] *= neutral_bias
     print(f"Class weights: {class_weights_dict}")
     sgd_classifier = SGDClassifier(loss='modified_huber', class_weight=class_weights_dict)
     pipeline = Pipeline([
@@ -137,11 +158,27 @@ def handler(event, context):
 
     classifier.partial_fit(X_transformed, y, classes=classes)
 
+  if is_test_run:
+    liked_articles = data_for_testing[data_for_testing['isliked'] == True]
+    liked_articles_predictions = pipeline.predict(liked_articles)
+    wrong_liked_articles_predictions = (liked_articles_predictions != 1).sum()
+    num_of_liked_articles = len(liked_articles)
+    print(f"Number of wrong predictions for liked articles: {wrong_liked_articles_predictions/num_of_liked_articles} {wrong_liked_articles_predictions}/{num_of_liked_articles}")
+    disliked_articles = data_for_testing[data_for_testing['isliked'] == False]
+    disliked_articles_predictions = pipeline.predict(disliked_articles)
+    wrong_disliked_articles_predictions = (disliked_articles_predictions != -1).sum()
+    num_of_disliked_articles = len(disliked_articles)
+    print(f"Number of wrong predictions for disliked articles: {wrong_disliked_articles_predictions/num_of_disliked_articles} {wrong_disliked_articles_predictions}/{num_of_disliked_articles}")
+    neutral_articles = data_for_testing[data_for_testing['isliked'].isna()]
+    neutral_articles_predictions = pipeline.predict(neutral_articles)
+    wrong_neutral_articles_predictions = (neutral_articles_predictions != 0).sum()
+    num_of_neutral_articles = len(neutral_articles)
+    print(f"Number of wrong predictions for neutral articles: {wrong_neutral_articles_predictions/num_of_neutral_articles} {wrong_neutral_articles_predictions}/{num_of_neutral_articles}")
 
   # Save the model and vectorizer back to S3
   print('Saving the data in S3')
   joblib.dump(pipeline, pipeline_full_filename)
-  if not IS_LOCAL:
+  if not is_test_run:
     s3.upload_file(pipeline_full_filename, bucket_name, f'{userId}/{pipeline_filename}')
   else:
     print('Skipping S3 upload')
